0a6e5e466180029c9949b103addcd473af317b41
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-14 14:32:50 -0300

    pegando todas as notícias principais

--- scraper_noticias.py
+++ scraper_noticias.py
@@ -8,3 +8,9 @@ navegador.get("https://g1.globo.com/")
 
 # colocar o navegador em tela cheia
 navegador.maximize_window()
+
+# selecionar um elemento na tela
+noticias = navegador.find_elements("css selector", 'p[data-mrf-layout-title]')
+
+for noticia in noticias:
+    print(noticia.text)
