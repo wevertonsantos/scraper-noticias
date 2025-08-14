43bda82d57327151c9f7bebbf12799301c3ea3b0
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-14 14:13:44 -0300

    acessando site e colocando em tela cheia

--- scraper_noticias.py
+++ scraper_noticias.py
@@ -2,3 +2,9 @@ from selenium import webdriver
 
 # abrir o navegador
 navegador = webdriver.Chrome()
+
+# acessar um site
+navegador.get("https://g1.globo.com/")
+
+# colocar o navegador em tela cheia
+navegador.maximize_window()
