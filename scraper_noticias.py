f2435e32f1c4884f2bc840636a74ba14de864ff0
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-14 14:41:27 -0300

    armazenando noticias no dicionário e salvando em arquivo txt

--- scraper_noticias.py
+++ scraper_noticias.py
@@ -12,5 +12,15 @@ navegador.maximize_window()
 # selecionar um elemento na tela
 noticias = navegador.find_elements("css selector", 'p[data-mrf-layout-title]')
 
+dados_noticias = {}
+
 for noticia in noticias:
-    print(noticia.text)
+    # armazenando notícias no dic
+    dados_noticias[f"noticia_{noticias.index(noticia)}"] = noticia.text
+
+with open("Principais notícias do dia.txt","w") as arquivo:
+    for dado_noticia in dados_noticias:
+        if dado_noticia == list(dados_noticias)[-1]:
+            arquivo.write(f"{dado_noticia}:{dados_noticias[dado_noticia]}")
+        else:
+            arquivo.write(f"{dado_noticia}:{dados_noticias[dado_noticia]}\n")
